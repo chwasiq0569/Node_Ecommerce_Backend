4c3419e5606191877e0ac2541a9010e167aa0792
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-03 14:14:51 +0500

    Created SignUp functionality

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,18 +1,29 @@
 const User = require("../models/user");
+const bcrypt = require('bcrypt');
 
-module.exports.signUp = (req, res) => {
-    const userData = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        role: req.body.role
-    }
-    const user = new User(userData);
-    user.save().then((_user) => {
-        console.log(_user)
-        res.json({_user})
-    }).catch((err) => {
-        console.log(err)
-        res.json({err})
-    })
+module.exports.signUp = async (req, res) => {
+    const userExists = await User.findOne({ email: req.body.email })
+        if(!userExists){
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            const userData = {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+                role: req.body.role
+            }
+            const user = new User(userData);
+            user.save().then((_user) => {
+                return res.status(201).json({ 
+                    message: "User Created Successfully",
+                    user: _user
+                 })
+            }).catch((err) => {
+                return res.status(201).json({
+                    message: "Something went wrong"
+                })
+            })
+        }else{
+            return res.status(400).json({'err': "User Already Exists"})
+        }
 }
+
